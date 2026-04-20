a1413b59a736d0bab85d4b6cc738992a094fbd7b
GAS <nikitashaitor@yandex.ru>
2026-04-20 11:33:13 +0300

    fix homework02

--- src/main/java/ArrayToList.java
+++ src/main/java/ArrayToList.java
@@ -2,11 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayToList {
-    public static void main(String[] args) {
-        String[] array = {"Apple", "Banana", "Chery"};
-        List<String> immutableList = List.of(array);
-        ArrayList<String> mutableList = new ArrayList<>(immutableList);
+    public static <T> ArrayList<T> toArrayList(T[] array) {
+        return new ArrayList<>(List.of(array));
+    }
 
+    public static void main(String[] args) {
+        String[] array = {"Apple", "Banana", "Cherry"};
+        ArrayList<String> mutableList = toArrayList(array);
         System.out.println(mutableList);
     }
 }
+
