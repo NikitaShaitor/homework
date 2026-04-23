fab5a61590a770f81122d129b796e4032f835b12
GAS <nikitashaitor@yandex.ru>
2026-04-23 09:09:50 +0300

    fix homework3

--- src/main/java/ru/otus/TestRunner.java
+++ src/main/java/ru/otus/TestRunner.java
@@ -1,41 +1,41 @@
 package ru.otus;
 
-import java.lang.annotation.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.annotation.*;
 
 public class TestRunner {
-
-    private static final List<TestResult> testResults = new ArrayList<>();
 
     public static void run(Class<?> testClass) {
         try {
             Method[] methods = testClass.getDeclaredMethods();
 
-            List<Method> beforeMethods = getAnnotatedMethods(methods, (Class<? extends Annotation>) Before.class);
-            List<Method> afterMethods = getAnnotatedMethods(methods, (Class<? extends Annotation>) After.class);
-            List<Method> testMethods = getAnnotatedMethods(methods, (Class<? extends Annotation>) Test.class);
+            List<Method> beforeMethods = getAnnotatedMethods(methods, Before.class);
+            List<Method> afterMethods = getAnnotatedMethods(methods, After.class);
+            List<Method> testMethods = getAnnotatedMethods(methods, Test.class);
 
-            for(Method testMethod : testMethods) {
+            List<TestResult> currentTestResults = new ArrayList<>();
+
+            for (Method testMethod : testMethods) {
                 Object instance = testClass.newInstance();
 
                 executeMethods(instance, beforeMethods);
-                executeSingleMethod(instance, testMethod);
+                executeSingleMethod(instance, testMethod, currentTestResults);
                 executeMethods(instance, afterMethods);
             }
 
-            printStatistics();
-        } catch(Exception e) {
+            printStatistics(currentTestResults);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private static List<Method> getAnnotatedMethods(Method[] methods, Class<? extends Annotation> annotationClass) {
         List<Method> result = new ArrayList<>();
-        for(Method m : methods) {
-            if(m.isAnnotationPresent(annotationClass)) {
+        for (Method m : methods) {
+            if (m.isAnnotationPresent(annotationClass)) {
                 result.add(m);
             }
         }
@@ -43,27 +43,29 @@ public class TestRunner {
     }
 
     private static void executeMethods(Object instance, List<Method> methods) throws InvocationTargetException, IllegalAccessException {
-        for(Method m : methods) {
+        for (Method m : methods) {
             m.invoke(instance);
         }
     }
 
-    private static void executeSingleMethod(Object instance, Method method) {
+    private static void executeSingleMethod(Object instance, Method method, List<TestResult> testResults) {
         try {
             method.invoke(instance);
             testResults.add(new TestResult(method.getName(), true));
-        } catch(Exception e) {
+        } catch (Exception e) {
             testResults.add(new TestResult(method.getName(), false));
         }
     }
 
-    private static void printStatistics() {
+    private static void printStatistics(List<TestResult> testResults) {
         long totalTests = testResults.size();
-        long passedTests = testResults.stream().filter(TestResult::isPassed).count();
+        long passedTests = testResults.stream()
+                .filter(TestResult::isPassed)
+                .count();
         long failedTests = totalTests - passedTests;
 
         System.out.println("\n=== Статистика тестов ===");
-        System.out.printf("Всего тестов: %d\nПрошло успешно: %d\nУпали: %d%n",
+        System.out.printf("Всего тестов: %d\nПрошло успешно: %d\nУпало: %d%n",
                 totalTests, passedTests, failedTests);
     }
 
